0547e5ce1c8d83c150dbc57cbc2b88b9d638f0c2
Joyce-Nkengbeza <Joyce.nkengbeza@stu.ocsb.ca>
2025-06-08 16:59:29 +0000

    fixed

--- js/script.js
+++ js/script.js
@@ -1,50 +1,68 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Joyce Nkengbeza
-// Created on: April 2025
+// Created on: May 2025
 // This file contains the JS functions for index.html
 
 "use strict"
 
-function isPrime(num) {
-  if (num < 2) return false
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false
-  }
-  return true
-}
+// This function checks if a number is prime
+function isPrime(number) {
+  let count = 0
+  let loopCounter = 1
 
-// Multiply using addition
-function multiplyByAddition(a, b) {
-  let result = 0
-  for (let i = 0; i < b; i++) {
-    result += a
-  }
-  return result
-}
-
-function calculate() {
-  const limit = parseInt(document.getElementById("limit").value)
-  let primes = []
-  let product = 1
-  let showSteps = "1"
-
-  for (let i = 2; i <= limit; i++) {
-    if (isPrime(i)) {
-      primes.push(i)
-      product = multiplyByAddition(product, i)
-      showSteps += " × " + i
+  while (loopCounter <= number) {
+    if (number % loopCounter === 0) {
+      count = count + 1
     }
+    loopCounter = loopCounter + 1
   }
 
-  if (primes.length > 0) {
+  if (count === 2) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// This function multiplies two numbers using addition
+function multiplyByAdding(a, b) {
+  let total = 0
+  let counter = 0
+
+  while (counter < b) {
+    total = total + a
+    counter = counter + 1
+  }
+
+  return total
+}
+
+// This function finds prime numbers and multiplies them
+function calculate() {
+  // input
+  const limit = parseInt(document.getElementById("limit").value)
+  let number = 2
+  let product = 1
+  let steps = "1"
+  let primeList = ""
+
+  // process
+  while (number <= limit) {
+    if (isPrime(number) === true) {
+      product = multiplyByAdding(product, number)
+      steps = steps + " × " + number
+      primeList = primeList + number + ", "
+    }
+    number = number + 1
+  }
+
+  // output
+  if (primeList !== "") {
     document.getElementById("results").innerHTML =
-      "Prime numbers: " +
-      primes.join(", ") +
-      "<br>Multiplication: " +
-      showSteps +
-      "<br>Final product: " +
-      product
+      "Prime numbers: " + primeList.slice(0, -2) +
+      "<br>Steps: " + steps +
+      "<br>Final product: " + product
   } else {
     document.getElementById("results").innerHTML =
       "There are no prime numbers in that range."
