311749188bc20c6e3a9f529d096d3796a430106a
Joyce-Nkengbeza <Joyce.nkengbeza@stu.ocsb.ca>
2025-06-16 18:54:49 +0000

    fixed

--- js/script.js
+++ js/script.js
@@ -1,68 +1,57 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Joyce Nkengbeza
-// Created on: May 2025
-// This file contains the JS functions for index.html
-
 "use strict"
 
-// This function checks if a number is prime
-function isPrime(number) {
-  let count = 0
-  let loopCounter = 1
-
-  while (loopCounter <= number) {
-    if (number % loopCounter === 0) {
-      count = count + 1
-    }
-    loopCounter = loopCounter + 1
-  }
-
-  if (count === 2) {
-    return true
-  } else {
-    return false
-  }
-}
-
-// This function multiplies two numbers using addition
-function multiplyByAdding(a, b) {
-  let total = 0
-  let counter = 0
-
-  while (counter < b) {
-    total = total + a
-    counter = counter + 1
-  }
-
-  return total
-}
-
-// This function finds prime numbers and multiplies them
 function calculate() {
   // input
-  const limit = parseInt(document.getElementById("limit").value)
+  let limit = parseInt(document.getElementById("limit").value)
   let number = 2
   let product = 1
   let steps = "1"
-  let primeList = ""
+  let primeText = ""
+  let commaNeeded = false
 
-  // process
   while (number <= limit) {
-    if (isPrime(number) === true) {
-      product = multiplyByAdding(product, number)
-      steps = steps + " × " + number
-      primeList = primeList + number + ", "
+    // Check if 'number' is prime:
+    let count = 0
+    let testNumber = 1
+
+    while (testNumber <= number) {
+      if (number % testNumber === 0) {
+        count = count + 1
+      }
+      testNumber = testNumber + 1
+    }
+
+    if (count === 2) {
+      // Multiply product by number using addition:
+      let total = 0
+      let counter = 0
+      while (counter < product) {
+        total = total + number
+        counter = counter + 1
+      }
+      product = total
+
+      // Prepare output strings
+      steps = steps + " x " + number
+
+      if (commaNeeded === true) {
+        primeText = primeText + ", "
+      }
+      primeText = primeText + number
+      commaNeeded = true
     }
     number = number + 1
   }
 
   // output
-  if (primeList !== "") {
+  if (primeText !== "") {
     document.getElementById("results").innerHTML =
-      "Prime numbers: " + primeList.slice(0, -2) +
-      "<br>Steps: " + steps +
-      "<br>Final product: " + product
+      "Prime numbers: " +
+      primeText +
+      "<br>Steps: " +
+      steps +
+      "<br>Final product: " +
+      product
   } else {
     document.getElementById("results").innerHTML =
       "There are no prime numbers in that range."
